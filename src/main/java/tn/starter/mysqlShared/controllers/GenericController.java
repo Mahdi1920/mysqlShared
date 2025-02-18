e66f6c6f1a6ee65bfd2cfec6b7fae8e4f986c7c1
mahdi <mahdichabbouh98@gmail.com>
2025-02-18 16:59:12 +0100

    discovery server eureka clients

--- src/main/java/tn/starter/mysqlShared/controllers/GenericController.java
+++ src/main/java/tn/starter/mysqlShared/controllers/GenericController.java
@@ -3,7 +3,6 @@ package tn.starter.mysqlShared.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.starter.mysqlShared.services.IGenericService;
-
 import java.util.List;
 
 public class GenericController<T> {
